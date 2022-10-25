d87fa6b1f87050003f81560feee986c95cbbdf08
John-David Noguera <JNogueraD94@gmail.com>
2022-10-25 16:56:34 -0400

    Add User/Product backend routes

---
+++ server/api/products.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const {
+  models: { Product },
+} = require("../db");
+
+// GET /api/products
+router.get("/", async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST /api/products
+router.post("/", async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET /api/products/:id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    res.json(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/products/:id
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.send(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
