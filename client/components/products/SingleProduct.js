2102d730190dd8b864c8ee9ffbd97ed9c605d5a6
Corinna <Corinnadejesus@gmail.com>
2022-10-26 13:16:44 -0400

    singleProduct bug

---
+++ client/components/products/SingleProduct.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSingleProduct } from "../../features/productsSlice";
+
+function SingleProduct() {
+  const dispatch = useDispatch();
+  const { id } = useParams;
+
+  const product = useSelector((state) => state.products.product);
+  console.log("product", product);
+
+  useEffect(() => {
+    dispatch(fetchSingleProduct(id));
+  }, [dispatch]);
+
+  // key={product.id}
+  return (
+    <ul>
+      <p>SINGLE PRODUCT AREA</p>;
+      {/* <div key={product.id} className="product-label">
+            <img
+              src={product.imageUrl}
+            />
+            <p>Name: {product.name}</p>
+            <p>{product.description}</p>
+            <p>Price: {product.price}</p>
+            <p>{product.genre}</p>
+            <p>In Stock: {product.inventory}</p>
+          </div> */}
+    </ul>
+  );
+}
+
+// (all products display)
+//image, name, description, price
+
+//(single product display)
+//image
+//id
+//name
+//description
+//price
+//inventory
+//genre
+
+export default SingleProduct;
--- client/features/productsSlice.js
+++ client/features/productsSlice.js
@@ -1,35 +1,47 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchProducts = createAsyncThunk("fetchProducts", async () => {
-  try {
-    const { data } = await axios.get("/api/products");
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
-});
-
-export const addProduct = createAsyncThunk(
-  "addProduct",
-  async (name, image, description) => {
-    const { data } = await axios.post(
-      "/api/products",
-      name,
-      image,
-      description
-    );
-    return data;
+export const fetchAllProducts = createAsyncThunk(
+  "fetchAllProducts",
+  async () => {
+    try {
+      const { data } = await axios.get("/api/products");
+      return data;
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 );
 
-export const deleteProduct = createAsyncThunk(
-  "deleteProduct",
-  async (productId) => {
-    const { data } = await axios.delete(`/api/products/${productId}`);
-    return data;
+export const fetchSingleProduct = createAsyncThunk(
+  "fetchSingleProduct",
+  async (id) => {
+    try {
+      const { data } = await axios.get(`/api/products/${id}`);
+      return data;
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 );
+
+// export const addProduct = createAsyncThunk(
+//   "addProduct",
+//   async (name, image, description) => {
+//     const { data } = await axios.post(
+//       "/api/products",
+//       name,
+//       image,
+//       description
+//     );
+//     return data;
+//   }
+// );
+
+// export const deleteProduct = createAsyncThunk("deleteProduct", async (id) => {
+//   const { data } = await axios.delete(`/api/products/${id}`);
+//   return data;
+// });
 
 export const productsSlice = createSlice({
   name: "products",
@@ -39,19 +51,21 @@ export const productsSlice = createSlice({
   },
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchProducts.fulfilled, (state, action) => {
-      console.log("WOOOOOOOOO");
+    builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
       state.products = action.payload;
     }),
-      builder.addCase(addProduct.fulfilled, (state, action) => {
-        state.push(action.payload);
-      }),
-      builder.addCase(deleteProduct.fulfilled, (state, action) => {
-        const newProduct = state.filter(
-          (product) => product.id !== action.payload.id
-        );
-        return newProduct;
+      builder.addCase(fetchSingleProduct.fulfilled, (state, action) => {
+        console.log("payload", action.payload);
       });
+    // builder.addCase(addProduct.fulfilled, (state, action) => {
+    //   state.push(action.payload);
+    // }),
+    // builder.addCase(deleteProduct.fulfilled, (state, action) => {
+    //   const newProduct = state.filter(
+    //     (product) => product.id !== action.payload.id
+    //   );
+    //   return newProduct;
+    // });
   },
 });
 
