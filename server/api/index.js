6f7ed0cf0165072a905697a7c1836f74bec4fc1b
John-David Noguera <JNogueraD94@gmail.com>
2022-10-26 11:15:19 -0400

    modified index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
-module.exports = router;
 
 router.use("/users", require("./users"));
-router.use("/order", require("./order"));
-router.use("/wishlist", require("./wishlist"));
+// router.use("/order", require("./order"));
+// router.use("/wishlist", require("./wishlist"));
 
 router.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
+
+module.exports = router;
