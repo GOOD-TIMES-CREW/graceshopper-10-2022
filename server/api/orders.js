aa43a9eb6fca6da6d7d5a9bb8464741da2aec5a9
John-David Noguera <JNogueraD94@gmail.com>
2022-10-26 20:44:46 -0400

    backend order routes done

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
+const { default: axios } = require("axios");
 const {
   models: { Order, Order_Product },
 } = require("../db");
 
+// GET route for allOrders
+router.get("/", async (req, res, next) => {
+  try {
+    const allOrders = await Order.findAll();
+    res.json(allOrders);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// GET route for singleOrder
+router.get("/:id", async (req, res, next) => {
+  try {
+    const singleOrder = await Order.findByPk(req.params.id);
+    res.json(singleOrder);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// /api/orders/user/:userId
 router.get("/user/:userId", async (req, res, next) => {
   try {
     const order = await Order.findAll({
