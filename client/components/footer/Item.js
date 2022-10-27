e0abcad92174fb890f33278a64d052b4b6579215
Corinna <Corinnadejesus@gmail.com>
2022-10-27 08:37:11 -0400

    footer grid complete

--- client/components/footer/Item.js
+++ client/components/footer/Item.js
@@ -3,10 +3,14 @@ import React from "react";
 const Item = ({ Links, title }) => {
   return (
     <ul>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       {Links.map((link) => (
         <li key={link.name}>
-          <a href={link.link}>{link.name}</a>
+          <a href={link.link}>
+            {link.name}
+            {link.address}
+            {link.hours}
+          </a>
         </li>
       ))}
     </ul>
--- client/components/footer/Menus.js
+++ client/components/footer/Menus.js
@@ -1,25 +1,29 @@
 export const ABOUT = [
   { name: "Accessibility", link: "#" },
+  { name: "Customer Stories", link: "#" },
   { name: "Affiliates", link: "#" },
   { name: "Careers", link: "#" },
 ];
 export const CONTACT = [
-  { name: "Industries and tools", link: "#" },
-  { name: "Use cases", link: "#" },
+  { name: "Feedback", link: "#" },
+  { name: "Recall Notices", link: "#" },
   { name: "Blog", link: "#" },
-  { name: "Online evenet", link: "#" },
-  { name: "Nostrud exercitation", link: "#" },
+  { name: "Refund & Order Status", link: "#" },
 ];
 export const LEGAL = [
-  { name: "Diversity & inclusion", link: "#" },
-  { name: "About us", link: "#" },
+  { name: "Diversity & Inclusion Policy", link: "#" },
+  { name: "Terms & Conditions", link: "#" },
   { name: "Press", link: "#" },
-  { name: "Customer Stories", link: "#" },
-  { name: "Online communities", link: "#" },
+  { name: "Private Policy", link: "#" },
 ];
-export const COMPANY = [
-  { name: "Documentation", link: "#" },
-  { name: "Tutorials & guides", link: "#" },
-  { name: "Webinars", link: "#" },
-  { name: "Open-source", link: "#" },
+export const STORE = [
+  {
+    name: "Hours: ",
+    hours: `Monday: 11:00AM-6:00PM | Tuesday: 11:00AM-6:00PM | Wednesday: 11:00AM-6:00PM | Thursday: 11:00AM-6:00PM | Friday: 11:00AM-9:00PM | Saturday: 11:00AM-9:00PM | Sunday: Closed`,
+  },
+  {
+    name: "Location: ",
+    address: `13-02 Lakeview Road, Garden City, NY, 11506`,
+  },
+  { name: "Store Directions", link: "#" },
 ];
