b3954742757c2f246ad2eb27f454f168ccdd01b2
Alan Espinal <alanedespinal@gmail.com>
2022-10-28 12:28:03 -0400

    added links to all users page so that admin can access user info

---
+++ client/features/user/AllUsers.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "./userSlice";
+import { Link } from "react-router-dom";
+import AccountPage from "./AccountPage";
+
+function AllUsers() {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.users.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+  return (
+    <div className="all-users">
+      <h1>All Users</h1>
+      {users?.map((user) => (
+        <div className="user-container">
+          <h3>
+            <Link to={`/users/${user.id}`}>
+              {user.firstName} {user.lastName}
+            </Link>
+          </h3>
+          <p>{user.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AllUsers;
